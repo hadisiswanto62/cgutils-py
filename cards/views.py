08414b0b3a8cc7328daa420bf81ce2e35e2079b6
hadisiswanto62 <hadi0006@e.ntu.edu.sg>
2023-03-14 07:57:17 +0800

    card list now by default is filtered to only SSR+

--- cards/views.py
+++ cards/views.py
@@ -1,18 +1,30 @@
+from django.http.request import HttpRequest
 from django.views.generic.list import ListView
 from django.db.models import QuerySet
 from django_filters import FilterSet
-from .models import Card
-
+from .models import Card, Rarity
 
 class CardFilter(FilterSet):
+    DEFAULT_FILTER = {
+        "rarity": Rarity.SSRPlus.value,
+    }
     class Meta:
         model = Card
-        fields = ["attribute"]
+        fields = ["rarity"]
+
+    def __init__(self, data=None, *args, **kwargs):
+        if data is not None:
+            data = data.copy()
+            for name in self.base_filters.keys():
+                if name not in data and name in self.DEFAULT_FILTER:
+                    data[name] = self.DEFAULT_FILTER[name]
+
+        super().__init__(data, *args, **kwargs)
 
 
 class CardListView(ListView):
     model = Card
-    paginate_by = 25
+    paginate_by = 50
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
