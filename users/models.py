0ce6d530d16c94c7323d8cc5b50aad3a0409aa7e
hadisiswanto62 <hadi0006@e.ntu.edu.sg>
2023-03-14 07:57:43 +0800

    added team model (not correct)

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 from cards.models import Card
 
@@ -13,3 +14,17 @@ class OwnedCard(models.Model):
     card = models.ForeignKey(Card, on_delete=models.CASCADE)
     level = models.IntegerField()
     skill_level = models.IntegerField()
+
+class Team(models.Model):
+    name = models.TextField()
+    cards = models.ManyToManyField(to=OwnedCard)
+
+    def clean(self):
+        if self.cards.count() > 5:
+            raise ValidationError("Team has too many cards")
+        super().clean()
+
+    def save(self, *args, **kwargs):
+        if self.id is not None:
+            self.full_clean()
+        super().save(*args, **kwargs)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+from cards.tests import _create_card, _create_chara
+from model_bakery import baker
 
-# Create your tests here.
+from .models import UserData, OwnedCard, Team
+
+class TeamTest(TestCase):
+    def test_team_maxcard(self):
+        u = baker.make("auth.User")
+        ud = UserData.objects.get(user=u)
+        ocs = [baker.make("users.OwnedCard", user_data=ud)
+                for _ in range(6)]
+        team = Team.objects.create(name="test")
+        for oc in ocs[:5]:
+            team.cards.add(oc)
+        team.save()
+
+        team.cards.add(ocs[-1])
+        with self.assertRaises(ValidationError):
+            team.save()
